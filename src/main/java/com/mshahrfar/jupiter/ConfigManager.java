7c3d44bcfe633b05058e48936f0cdac60163c048
Pejman Ghorbanzade <pejman@ghorbanzade.com>
2017-03-28 20:02:32 -0500

    Added new classes to support main.properties
    - Config
    - ConfigManager
    - FatalException

---
+++ src/main/java/com/mshahrfar/jupiter/ConfigManager.java
@@ -0,0 +1,52 @@
+//
+// Jupiter: A Ride-Sharing Network Generation and Analysis Application
+// Copyright 2017 Mariam Shahrabifarahani <mshahrfar@gmail.com>
+// Released under the terms of MIT License
+// https://github.com/mshahrfar/jupiter/blob/master/LICENSE
+//
+
+package com.mshahrfar.jupiter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Constructing a configuration object is expensive as it may require
+ * loading contents of a file. This class provides a static method that
+ * ensures we construct the configuration object only once and still have
+ * access to that object from within any class. This design allows support
+ * for multiple configuration objects at the same time.
+ *
+ * @author Pejman Ghorbanzade
+ * @see Config
+ */
+public final class ConfigManager {
+
+  private static final Map<String, Config> hm = new HashMap<String, Config>();
+
+  /**
+   * This static method allows client to access configurations of a file
+   * while preventing extensive construction of a configuration object
+   * if the file has previously been asked for.
+   *
+   * @param filename the name of the file containing configuration
+   * @return a config object that holds properties according to given file
+   */
+  public static Config get(String filename) {
+    if (hm.containsKey(filename)) {
+      return hm.get(filename);
+    } else {
+      Config cfg = new Config(filename);
+      hm.put(filename, cfg);
+      return cfg;
+    }
+  }
+
+  /**
+   * Prevent instantiation of this class.
+   */
+  private ConfigManager() {
+    // intentionally left blank
+  }
+
+}
---
+++ src/main/java/com/mshahrfar/jupiter/FatalException.java
@@ -0,0 +1,31 @@
+//
+// Jupiter: A Ride-Sharing Network Generation and Analysis Application
+// Copyright 2017 Mariam Shahrabifarahani <mshahrfar@gmail.com>
+// Released under the terms of MIT License
+// https://github.com/mshahrfar/jupiter/blob/master/LICENSE
+//
+
+package com.mshahrfar.jupiter;
+
+import org.apache.log4j.Logger;
+
+/**
+ * This class defines a custom exception that is thrown to hint the main
+ * thread that the program should be aborted.
+ *
+ * @author Pejman Ghorbanzade
+ */
+public final class FatalException extends RuntimeException {
+
+  private static final Logger log = Logger.getLogger(FatalException.class);
+
+  /**
+   * A fatal exception updates the log file with the class name that caused it.
+   *
+   * @param cls the class from which this exception is thrown
+   */
+  public FatalException(Class<?> cls) {
+    log.fatal("fatal exception in class " + cls.getSimpleName());
+  }
+
+}
