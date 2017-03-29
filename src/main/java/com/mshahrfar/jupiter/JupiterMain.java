ca3ed7a7b5d7c79e855e7a04a33c4a7a2fa372c3
Pejman Ghorbanzade <pejman@ghorbanzade.com>
2017-03-28 22:13:36 -0500

    added google maps library and a basic sketch of its usage

--- src/main/java/com/mshahrfar/jupiter/JupiterMain.java
+++ src/main/java/com/mshahrfar/jupiter/JupiterMain.java
@@ -9,8 +9,19 @@ package com.mshahrfar.jupiter;
 
 import org.apache.log4j.Logger;
 
+// should be moved to another class
+import com.google.maps.DirectionsApi;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.DirectionsLeg;;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.DirectionsRoute;
+import com.google.maps.model.LatLng;
+import com.google.maps.model.TravelMode;
+
 /**
- *
+ * Main class of Jupiter.
  *
  * @author Mariam Shahrabifarahani
  * @author Pejman Ghorbanzade
@@ -28,6 +39,33 @@ public class JupiterMain {
      */
     public static void main(String[] args) {
         log.info("Hello from Jupiter");
+        Config cfg = ConfigManager.get("config/main.properties");
+
+        String apiKey = cfg.getAsString("google.map.api.key");
+        // context is expensive and should be declared as static var
+        GeoApiContext context = new GeoApiContext().setApiKey(apiKey);
+        DirectionsApiRequest request = DirectionsApi.newRequest(context);
+        request.origin(new LatLng(40.644737, -73.781937));
+        request.destination(new LatLng(40.811108, -73.957993));
+        request.mode(TravelMode.DRIVING);
+        try {
+            DirectionsResult result = request.await();
+            for (DirectionsRoute route: result.routes) {
+                long distance = 0;
+                long duration = 0;
+                for (DirectionsLeg leg: route.legs) {
+                    duration += leg.duration.inSeconds;
+                    distance += leg.distance.inMeters;
+                }
+                log.info("total duration is " + duration);
+                log.info("total distance is " + distance);
+            }
+        } catch (ApiException exp) {
+            log.error(exp.getMessage());
+        } catch (Exception exp) {
+            log.error(exp.getMessage());
+        }
+
     }
 
     /**
