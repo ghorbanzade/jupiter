5098d0cc6474cc48f7a96cbca73f48adc29b4c43
mshahrfar <mshahrfar@gmail.com>
2017-04-03 12:49:53 -0500

    added InputRule and TimeWindowRule

---
+++ src/main/java/com/mshahrfar/jupiter/InputRule.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.mshahrfar.jupiter;
+
+/**
+ * @author mshah
+ *
+ */
+public abstract class InputRule {
+	public InputRule () {
+		
+	}
+	private List<Customer> list;
+}
---
+++ src/main/java/com/mshahrfar/jupiter/TimeWindowRule.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.mshahrfar.jupiter;
+
+/**
+ * @author Mariam
+ *
+ */
+public class TimeWindowRule extends InputRule {
+
+	/**
+	 * 
+	 */
+	public TimeWindowRule() {
+		// TODO Auto-generated constructor stub
+		
+	}
+
+}
