c8efff40c9f9b2b3c60ef20677f9730b1e4fc182
mshahrfar <mshahrfar@gmail.com>
2017-04-03 13:59:25 -0500

    added VicinityFilter

---
+++ src/main/java/com/mshahrfar/jupiter/VicinityFilter.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.mshahrfar.jupiter;
+
+/**
+ * @author mshah
+ *
+ */
+public class VicinityFilter {
+
+	/**
+	 * 
+	 */
+	public VicinityFilter() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
