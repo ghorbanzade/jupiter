1287d5b3b38718fb84d2854ef8cc4127a4fc6f74
Pejman Ghorbanzade <pejman@ghorbanzade.com>
2017-04-09 20:13:01 -0500

    added comment header to filter interface

--- src/main/java/com/mshahrfar/jupiter/Filter.java
+++ src/main/java/com/mshahrfar/jupiter/Filter.java
@@ -1,3 +1,10 @@
+//
+// Jupiter: A Ride-Sharing Network Generation and Analysis Application
+// Copyright 2017 Mariam Shahrabifarahani <mshahrfar@gmail.com>
+// Released under the terms of MIT License
+// https://github.com/mshahrfar/jupiter/blob/master/LICENSE
+//
+
 package com.mshahrfar.jupiter;
 
 import java.util.List;
