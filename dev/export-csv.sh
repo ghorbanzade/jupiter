0459e06ba5c6efca90a1fcf1c65aec988e8ba511
Pejman Ghorbanzade <pejman@ghorbanzade.com>
2017-04-28 16:00:01 -0500

    generate csv files in data/csv/ directory

--- dev/export-csv.sh
+++ dev/export-csv.sh
@@ -19,22 +19,22 @@ get_mongo_config () {
 
 task_export () {
     # by default, a local instance of mongoDB is used
+    local DIR_CSV='data/csv'
     local MONGODB_CONFIG=$(get_mongo_config "$@")
 
     # rides
-    mongoexport ${MONGODB_CONFIG} --db jupiter --collection rides --type=csv --out rides.csv --fields customer_id,rider_ids,duration_total,distance_total,t_p1_p2,t_p2_d2,t_d2_d1,t_p2_d1,t_d1_d2,d_p1_p2,d_p2_d2,d_d2_d1,d_p2_d1,d_d1_d2,scenario,candidates_count,candidate_ids
+    mongoexport ${MONGODB_CONFIG} --db jupiter --collection rides --type=csv --out ${DIR_CSV}/rides.csv --fields customer_id,rider_ids,duration_total,distance_total,t_p1_p2,t_p2_d2,t_d2_d1,t_p2_d1,t_d1_d2,d_p1_p2,d_p2_d2,d_d2_d1,d_p2_d1,d_d1_d2,scenario,candidates_count,candidate_ids
 
     # candidates
-    mongoexport ${MONGODB_CONFIG} --db jupiter --collection candidates --type=csv --out candidates.csv --fields customer_id,candidates_count,candidate_ids
+    mongoexport ${MONGODB_CONFIG} --db jupiter --collection candidates --type=csv --out ${DIR_CSV}/candidates.csv --fields customer_id,candidates_count,candidate_ids
 
     # single_rides
-    mongoexport ${MONGODB_CONFIG} --db jupiter --collection single_rides --type=csv --out single_rides.csv --fields customer_id,google_distance,google_duration
+    mongoexport ${MONGODB_CONFIG} --db jupiter --collection single_rides --type=csv --out ${DIR_CSV}/single_rides.csv --fields customer_id,google_distance,google_duration
 
     #mongodump ${MONGODB_CONFIG}
 }
 
 if [ $# -eq 0 ]; then
-    log_debug "no arguments provided"
     print_usage
     exit
 fi
