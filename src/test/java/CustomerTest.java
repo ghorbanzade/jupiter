d0864da07fb31c78ce55ca52cc29a56523111ba3
Pejman Ghorbanzade <pejman@ghorbanzade.com>
2019-03-08 01:11:44 -0600

    fixed more checkstyle warnings

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -13,12 +13,12 @@ import static org.hamcrest.Matchers.closeTo;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
-import org.junit.Test;
-
 import com.google.maps.model.LatLng;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+
+import org.junit.Test;
 
 /**
  *
@@ -26,50 +26,50 @@ import java.nio.file.Paths;
  */
 public class CustomerTest {
 
-    /**
-     *
-     */
-    @Test(expected = CustomerException.class)
-    public void makeDatasetParserWithInvalidPath() {
-        CustomerParser parser = new DatasetParser(Paths.get("nonexistant.csv"));
-    }
+  /**
+   *
+   */
+  @Test(expected = CustomerException.class)
+  public void makeDatasetParserWithInvalidPath() {
+    CustomerParser parser = new DatasetParser(Paths.get("nonexistant.csv"));
+  }
 
-    /**
-     *
-     */
-    @Test
-    public void parseSampleCSV() {
-        CustomerParser parser = new DatasetParser(Paths.get("data/sample.csv"));
-        assertThat(parser.hasNext(), is(true));
-        Customer customer = parser.next();
-        assertThat(customer.getId(), is(1L));
-        assertThat(customer.countPassengers(), is(1));
-        assertThat(customer.getPickupTime(), is(1401624000000L));
-        assertThat(customer.getIndividualRideDuration(), is(1200000L));
-        assertThat(customer.getPickupLocation().lat, is(40.6453743));
-        assertThat(customer.getDropoffLocation().lng, is(-73.9438858));
-        parser.close();
-        // parser should gracefully handle (and neglect) close instruction
-        // if it is already closed.
-        parser.close();
-    }
+  /**
+   *
+   */
+  @Test
+  public void parseSampleCsv() {
+    CustomerParser parser = new DatasetParser(Paths.get("data/sample.csv"));
+    assertThat(parser.hasNext(), is(true));
+    Customer customer = parser.next();
+    assertThat(customer.getId(), is(1L));
+    assertThat(customer.countPassengers(), is(1));
+    assertThat(customer.getPickupTime(), is(1401624000000L));
+    assertThat(customer.getIndividualRideDuration(), is(1200000L));
+    assertThat(customer.getPickupLocation().lat, is(40.6453743));
+    assertThat(customer.getDropoffLocation().lng, is(-73.9438858));
+    parser.close();
+    // parser should gracefully handle (and neglect) close instruction
+    // if it is already closed.
+    parser.close();
+  }
 
-    /**
-     *
-     */
-    @Test
-    public void checkCustomer() {
-        Customer customerA = new Customer(1);
-        Customer customerB = new Customer(2);
-        try {
-          Customer customerC = (Customer) customerA.clone();
-          assertThat(customerC, is(not(1)));
-          assertThat(customerC, is(customerA));
-          assertThat(customerC, is(not(customerB)));
-          assertThat(customerC.toString().equals("1"), is(true));
-        } catch (CloneNotSupportedException ex) {
-          fail("failed to clone customer");
-        }
+  /**
+   *
+   */
+  @Test
+  public void checkCustomer() {
+    Customer customerA = new Customer(1);
+    Customer customerB = new Customer(2);
+    try {
+      Customer customerC = (Customer) customerA.clone();
+      assertThat(customerC, is(not(1)));
+      assertThat(customerC, is(customerA));
+      assertThat(customerC, is(not(customerB)));
+      assertThat(customerC.toString().equals("1"), is(true));
+    } catch (CloneNotSupportedException ex) {
+      fail("failed to clone customer");
     }
+  }
 
 }
--- src/test/java/InputTest.java
+++ src/test/java/InputTest.java
@@ -11,13 +11,13 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
-import org.junit.rules.ExpectedException;
-import org.junit.Test;
-
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 /**
  *
@@ -26,61 +26,61 @@ import java.util.List;
  */
 public class InputTest {
 
-    /**
-     *
-     */
-    @Test
-    public void testTimeWindowInputBasics() {
-        CustomerParser parser = new DatasetParser(Paths.get("data/sample.csv"));
-        CustomerInput input = new TimeWindowInput(parser, 5, 5);
-        assertThat(input.hasCustomer(), is(true));
-        Customer customer = input.nextCustomer();
-        assertThat(customer.getId(), is(1L));
-        List<Long> candidateIds = new ArrayList<Long>();
-        input.getCandidates().iterator().forEachRemaining(
-            c -> candidateIds.add(c.getId())
-        );
-        assertThat(candidateIds, is(Arrays.asList(2L, 3L, 4L, 5L, 6L, 7L)));
-        //assertThat(input.nextCustomer().getId(), is(2L));
-        parser.close();
-    }
+  /**
+   *
+   */
+  @Test
+  public void testTimeWindowInputBasics() {
+    CustomerParser parser = new DatasetParser(Paths.get("data/sample.csv"));
+    CustomerInput input = new TimeWindowInput(parser, 5, 5);
+    assertThat(input.hasCustomer(), is(true));
+    Customer customer = input.nextCustomer();
+    assertThat(customer.getId(), is(1L));
+    List<Long> candidateIds = new ArrayList<Long>();
+    input.getCandidates().iterator().forEachRemaining(
+        c -> candidateIds.add(c.getId())
+    );
+    assertThat(candidateIds, is(Arrays.asList(2L, 3L, 4L, 5L, 6L, 7L)));
+    //assertThat(input.nextCustomer().getId(), is(2L));
+    parser.close();
+  }
 
-    /**
-     *
-     */
-    @Test
-    public void testFilter() {
-        CustomerParser parser = new DatasetParser(Paths.get("data/sample.csv"));
-        CustomerInput input = new TimeWindowInput(parser, 120, 120);
-        input.addFilter(new VicinityFilter(17000));
-        Customer customer = input.nextCustomer();
-        List<Long> ids = new ArrayList<Long>();
-        input.getCandidates().iterator().forEachRemaining(
-            c -> ids.add(c.getId())
-        );
-        assertThat(ids, is(Arrays.asList(20L)));
-        parser.close();
-    }
+  /**
+   *
+   */
+  @Test
+  public void testFilter() {
+    CustomerParser parser = new DatasetParser(Paths.get("data/sample.csv"));
+    CustomerInput input = new TimeWindowInput(parser, 120, 120);
+    input.addFilter(new VicinityFilter(17000));
+    Customer customer = input.nextCustomer();
+    List<Long> ids = new ArrayList<Long>();
+    input.getCandidates().iterator().forEachRemaining(
+        c -> ids.add(c.getId())
+    );
+    assertThat(ids, is(Arrays.asList(20L)));
+    parser.close();
+  }
 
-    /**
-     *
-     */
-    @Test
-    public void testExcludeCandidate() {
-        CustomerParser parser = new DatasetParser(Paths.get("data/sample.csv"));
-        CustomerInput input = new TimeWindowInput(parser, 120, 120);
-        input.addFilter(new VicinityFilter(17000));
-        input.excludeCandidate(new Customer(20L));
-        // input should gracefully handle exclusion of the same customer
-        // for a second time
-        input.excludeCandidate(new Customer(20L));
-        Customer customer = input.nextCustomer();
-        List<Long> ids = new ArrayList<Long>();
-        input.getCandidates().iterator().forEachRemaining(
-            c -> ids.add(c.getId())
-        );
-        assertThat(ids, is((new ArrayList<Customer>())));
-        parser.close();
-    }
+  /**
+   *
+   */
+  @Test
+  public void testExcludeCandidate() {
+    CustomerParser parser = new DatasetParser(Paths.get("data/sample.csv"));
+    CustomerInput input = new TimeWindowInput(parser, 120, 120);
+    input.addFilter(new VicinityFilter(17000));
+    input.excludeCandidate(new Customer(20L));
+    // input should gracefully handle exclusion of the same customer
+    // for a second time
+    input.excludeCandidate(new Customer(20L));
+    Customer customer = input.nextCustomer();
+    List<Long> ids = new ArrayList<Long>();
+    input.getCandidates().iterator().forEachRemaining(
+        c -> ids.add(c.getId())
+    );
+    assertThat(ids, is((new ArrayList<Customer>())));
+    parser.close();
+  }
 
 }
